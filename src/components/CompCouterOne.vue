5237f38ef45b641312612b155636da30057a950b
“GdsThanhTung” <gds.thanhtung@gmail.com>
2024-12-26 12:27:54 +0700

    Init

---
+++ src/components/CompCouterOne.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h2>Counter One</h2>
+    <p>{{ count }}</p>
+    <button @click="increment">++</button>
+    <button @click="decrease">--</button>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'CompCouterOne',
+    data() {
+      return {
+        count: 0
+      }
+    },
+    methods: {
+      increment() {
+        this.count++
+      },
+      decrease() {
+        this.count--
+      }
+    }
+  }
+</script>
---
+++ src/components/CompCouterTwo.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h2>Counter Two</h2>
+    <p>{{ count }}</p>
+    <button @click="increment">++</button>
+    <button @click="decrease">--</button>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'CompCouterTwo',
+    data() {
+      return {
+        count: 0
+      }
+    },
+    methods: {
+      increment() {
+        this.count++
+      },
+      decrease() {
+        this.count--
+      }
+    }
+  }
+</script>
